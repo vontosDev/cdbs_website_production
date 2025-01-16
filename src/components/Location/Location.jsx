6625a9f21e71c2a99cd2c769ffbfb162f8249b52
unknown <johnmark@vontos.co>
2025-01-16 14:24:20 +0800

    updated map address

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -5,18 +5,18 @@ function Location() {
         
         <div className="h-images-subhighlight mt-10 border border-black overflow-hidden p-4 mx-20">
   <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1930.6662712360596!2d121.0519528!3d14.2643558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd7d5e62432759%3A0xb7e4dc6ce6edb43b!2sLaguna%20Technopark%2C%20Inc.!5e0!3m2!1sen!2sph!4v1700000000000!5m2!1sen!2sph"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1931.150420530468!2d121.0535292!3d14.255288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd7d680d56e78d%3A0x3fb1fbaf74bb68bc!2sCaritas%20Don%20Bosco%20School!5e0!3m2!1sen!2sph!4v1700000000000!5m2!1sen!2sph"
     className="w-full h-full"
     style={{ border: "0" }}
     allowFullScreen=""
     loading="lazy"
     referrerPolicy="no-referrer-when-downgrade"
-    title="Laguna Technopark Map"
+    title="Caritas Don Bosco School Map"
   ></iframe>
 </div>
 
         <p className="text-3xl font-thin leading-10 mx-20 mt-10">
-        Laguna Blvd., Laguna Technopark, Brgy. Biñan, Biñan City, Laguna 4024
+        7354+G63, Laguna Technopark, Laguna Blvd, Biñan, 4024 Laguna
         </p>
       </div>
     );
